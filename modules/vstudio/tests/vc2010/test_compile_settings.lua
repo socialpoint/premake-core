a5833420e6b0299b32be8f724d12cf7ef6b09380
Nicholaus Clark <nicholaus.clark115@gmail.com>
2021-11-06 12:19:58 -0500

    Changed C++20 to emit C++20 instead of C++Latest in MSVC, Added C11 and C17
    # Conflicts:
    #	modules/vstudio/vs2010_vcxproj.lua
    #	website/docs/cdialect.md

--- modules/vstudio/tests/vc2010/test_compile_settings.lua
+++ modules/vstudio/tests/vc2010/test_compile_settings.lua
@@ -1244,6 +1244,21 @@
 		]]
 	end
 
+	function suite.onLanguage_Cpp20_VS2017()
+		p.action.set("vs2017")
+
+		cppdialect 'C++20'
+		prepare()
+		test.capture [[
+<ClCompile>
+	<PrecompiledHeader>NotUsing</PrecompiledHeader>
+	<WarningLevel>Level3</WarningLevel>
+	<Optimization>Disabled</Optimization>
+	<LanguageStandard>stdcpplatest</LanguageStandard>
+</ClCompile>
+		]]
+	end
+
 	function suite.onLanguage_Cpp20_VS2019()
 		p.action.set("vs2019")
 
@@ -1254,7 +1269,37 @@
 	<PrecompiledHeader>NotUsing</PrecompiledHeader>
 	<WarningLevel>Level3</WarningLevel>
 	<Optimization>Disabled</Optimization>
-	<LanguageStandard>stdcpplatest</LanguageStandard>
+	<LanguageStandard>stdcpp20</LanguageStandard>
+</ClCompile>
+		]]
+	end
+
+	function suite.onLanguage_C11_VS2019()
+		p.action.set("vs2019")
+
+		cdialect 'C11'
+		prepare()
+		test.capture [[
+<ClCompile>
+	<PrecompiledHeader>NotUsing</PrecompiledHeader>
+	<WarningLevel>Level3</WarningLevel>
+	<Optimization>Disabled</Optimization>
+	<LanguageStandard_C>stdc11</LanguageStandard_C>
+</ClCompile>
+		]]
+	end
+
+	function suite.onLanguage_C17_VS2019()
+		p.action.set("vs2019")
+
+		cdialect 'C17'
+		prepare()
+		test.capture [[
+<ClCompile>
+	<PrecompiledHeader>NotUsing</PrecompiledHeader>
+	<WarningLevel>Level3</WarningLevel>
+	<Optimization>Disabled</Optimization>
+	<LanguageStandard_C>stdc17</LanguageStandard_C>
 </ClCompile>
 		]]
 	end
